3b81f2ef7e909d4c0926d65ae865093557189d72
Stan Wang <stanley_wang_uni@yahoo.com>
2022-03-10 17:21:56 +0000

    Add files via upload

--- Labs/Module 2/init.sh
+++ Labs/Module 2/init.sh
@@ -11,7 +11,6 @@ ARM_SPN_CLIENT="tpcds-spn-client"
 ARM_SPN_TENANT="tpcds-spn-tenant"
 
 # Databricks Configuration
-MANAGEMENT_RESOURCE_ENDPOINT="https://management.core.windows.net/" 
 RESOURCE_GROUP="PoC-Synapse-Analytics"
 LOCATION="eastus"
 
@@ -54,6 +53,19 @@ echo "Assign the Resource Group Contributor Role to SPN"
 az role assignment create --assignee "$ARM_OBJECT_ID" \
 --role "Contributor" \
 --scope "/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP"
+
+echo "Assign the Key Vault Contributor Role to SPN"
+az role assignment create --assignee "$ARM_OBJECT_ID" \
+--role "Contributor" \
+--scope "/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.KeyVault/vaults/$KEY_VAULT"
+
+echo "Assign the Storage Blob Data Contributor Role to SPN"
+az role assignment create --assignee "$ARM_OBJECT_ID" \
+--role "Storage Blob Data Contributor" \
+--scope "/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Storage/storageAccounts/$STORAGE_ACCT"
+
+az keyvault set-policy -n $KEY_VAULT --secret-permissions all --application-id $ARM_CLIENT_ID --object-id $ARM_OBJECT_ID 
+
 
 echo "complete initialization ......."
 
