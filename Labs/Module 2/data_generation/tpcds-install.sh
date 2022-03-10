a964ab13671bd197f0beb3cf51c6c04d6db1dfa2
Stan Wang <stanley_wang_uni@yahoo.com>
2022-03-10 15:53:47 -0500

    Delete tpcds-install.sh

--- Labs/Module 2/data_generation/tpcds-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get --assume-yes install gcc make flex bison byacc git
-cd /usr/local/bin
-git clone https://github.com/databricks/tpcds-kit.git
-cd tpcds-kit/tools
-make OS=LINUX
