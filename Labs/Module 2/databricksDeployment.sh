eee1642ee6a445118fbab2322c80f9222c771fa9
Stan Wang <stanley_wang_uni@yahoo.com>
2022-03-10 13:09:51 +0000

    Add files via upload

--- Labs/Module 2/databricksDeployment.sh
+++ Labs/Module 2/databricksDeployment.sh
@@ -2,30 +2,64 @@
 
 # Get environment details
 export ARM_SUBSCRIPTION_ID=$(az account show --query id --output tsv 2>&1)
-export AZURE_DATABRICKS_APP_ID="2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
+export AZURE_DATABRICKS_APP_ID="2ff814a6-3304-4ab8-85cb-cd0e6f879c1d" 
 
-echo "Creating Service Principal for Databricks "
+# Key Vault Secret Info
+KEY_VAULT="tpcds-kv"
+ARM_SPN_CREDENTIAL="tpcds-spn-secret"
+ARM_SPN_OBJECT="tpcds-spn-object"
+ARM_SPN_CLIENT="tpcds-spn-client"
+ARM_SPN_TENANT="tpcds-spn-tenant"
 
-DATABRICKS_SPN_NAME="App-PoC-TPCDS-Databricks"
-# ARM_CLIENT_SECRET=$(az ad sp create-for-rbac --name "$DATABRICKS_SPN_NAME" --scopes /subscriptions/"$ARM_SUBSCRIPTION_ID" --query password -o tsv)
-ARM_CLIENT_SECRET="iwk.rdo1T1o1lJIowbKEI5C25edsG6yqOG"
-
-ARM_TENANT_ID=$(az ad sp list --display-name "$DATABRICKS_SPN_NAME" --query [].appOwnerTenantId -o tsv)
-ARM_CLIENT_ID=$(az ad sp list --display-name "$DATABRICKS_SPN_NAME" --query [].appId -o tsv)
-ARM_OBJECT_ID=$(az ad sp list --display-name "$DATABRICKS_SPN_NAME" --query [].objectId -o tsv)
-
+# Databricks Configuration
 MANAGEMENT_RESOURCE_ENDPOINT="https://management.core.windows.net/" 
 RESOURCE_GROUP="PoC-Synapse-Analytics"
 LOCATION="eastus"
 DATABRICKS_WORKSPACE="pocdatabricks-tpcds"
 DATABRICKS_CLUSTER_NAME="test-cluster-01"
-DATABRICKS_SPARK_VERSION="10.0-scala2.12"
+DATABRICKS_SPARK_VERSION="9.1.x-scala2.12"
 DATABRICKS_NODE_TYPE="Standard_D3_v2"
 DATABRICKS_NUM_WORKERS=4 
 DATABRICKS_SPARK_CONF='{"spark.speculation":"true","spark.databricks.delta.preview.enabled":"true"}'
 DATABRICKS_AUTO_TERMINATE_MINUTES=60
 
-echo "Assign the Contributor Role to SPN"
+echo "Creating Service Principal ......"
+
+APP_SPN_NAME="App-PoC-TPCDS-Test"
+ARM_CLIENT_SECRET=$(az ad sp create-for-rbac --name "$APP_SPN_NAME" --scopes /subscriptions/"$ARM_SUBSCRIPTION_ID" --query password -o tsv)
+#ARM_CLIENT_SECRET="iwk.rdo1T1o1lJIowbKEI5C25edsG6yqOG"
+
+ARM_TENANT_ID=$(az ad sp list --display-name "$APP_SPN_NAME" --query [].appOwnerTenantId -o tsv)
+ARM_CLIENT_ID=$(az ad sp list --display-name "$$APP_SPN_NAME" --query [].appId -o tsv)
+ARM_OBJECT_ID=$(az ad sp list --display-name "$APP_SPN_NAME" --query [].objectId -o tsv)
+
+echo "Creating Key Vault ......"
+
+az keyvault create --name $KEY_VAULT --resource-group $RESOURCE_GROUP --location $LOCATION
+
+echo "ARM_CLIENT_SECRET : $ARM_CLIENT_SECRET"
+
+az keyvault secret set --vault-name $KEY_VAULT --name $ARM_SPN_CREDENTIAL --value $ARM_CLIENT_SECRET
+
+az keyvault secret set --vault-name $KEY_VAULT --name $ARM_SPN_OBJECT --value $ARM_OBJECT_ID
+
+az keyvault secret set --vault-name $KEY_VAULT --name $ARM_SPN_CLIENT --value $ARM_CLIENT_ID
+
+az keyvault secret set --vault-name $KEY_VAULT --name $ARM_SPN_TENANT --value $ARM_TENANT_ID
+
+echo "ARM_OBJECT_ID : $ARM_OBJECT_ID"
+echo "$(az keyvault secret show --name $ARM_SPN_OBJECT --vault-name $KEY_VAULT --query value -o tsv)"
+
+echo "ARM_CLIENT_ID : $ARM_CLIENT_ID"
+echo "$(az keyvault secret show --name $ARM_SPN_CLIENT --vault-name $KEY_VAULT --query value -o tsv)"
+
+echo "ARM_TENANT_ID : $ARM_TENANT_ID"
+echo "$(az keyvault secret show --name $ARM_SPN_TENANT --vault-name $KEY_VAULT --query value -o tsv)"
+
+echo "ARM_CLIENT_SECRET : $ARM_CLIENT_SECRET"
+echo "$(az keyvault secret show --name $ARM_SPN_CREDENTIAL --vault-name $KEY_VAULT --query value -o tsv)"
+
+echo "Assign the Resource Group Contributor Role to SPN"
 az role assignment create --assignee "$ARM_OBJECT_ID" \
 --role "Contributor" \
 --scope "/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP"
@@ -45,7 +79,7 @@ fi
 # Enable install of extensions without prompt
 az config set extension.use_dynamic_install=yes_without_prompt
 
-echo "Creating Databricks Workspace ........" 
+echo "Creating Databricks Workspace, if not existing ........" 
 
 # Create databricks workspace using extenstion
 # The extension will automatically install the first time you run an az databricks workspace command
@@ -82,38 +116,74 @@ az_mgmt_resource_endpoint=$(curl -X GET -H 'Content-Type: application/x-www-form
 -d 'grant_type=client_credentials&client_id='$ARM_CLIENT_ID'&resource='$MANAGEMENT_RESOURCE_ENDPOINT'&client_secret='$ARM_CLIENT_SECRET \
 https://login.microsoftonline.com/$ARM_TENANT_ID/oauth2/token)
 
-# Extract the access_token value
+# Extract management access token value
 mgmt_access_token=$(jq .access_token -r <<< "$az_mgmt_resource_endpoint" )
 echo "Management Access Token: $mgmt_access_token"
 
-# Create PAT token valid for 5 min (300 sec)
-pat_token_response=$(curl -X POST \
-    -H "Authorization: Bearer $token" \
-    -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
-    -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" \
-    -d '{"lifetime_seconds": 300,"comment": "PAT token"}' \
-    https://$workspaceUrl/api/2.0/token/create
-)
+# Import Notebook into workspace
+pathOnDatabricks="/tpcds"
 
-# Print PAT token
-pat_token=$(jq .token_value -r <<< "$pat_token_response")
-echo "pat_token: $pat_token"
+JSON="{ \"path\" : \"$pathOnDatabricks\" }"
+echo "Creating Path: $JSON"    
+echo "curl https://$workspaceUrl/api/2.0/workspace/mkdirs --data $JSON"
 
-# List PAT tokens (OPTIONAL)
-curl -X GET \
-    -H "Authorization: Bearer $token" \
-    -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
-    -H "X-Databricks-Azure-Workspace-Resource-Id: $workspaceId" \
-    https://$workspaceUrl/api/2.0/token/list
+curl -X POST https://$workspaceUrl/api/2.0/workspace/mkdirs \
+        -H "Authorization:Bearer $token" \
+        -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
+        -H "X-Databricks-Azure-Workspace-Resource-Id: $workspaceId" \
+        -H "Content-Type: application/json" \
+        --data "$JSON"
 
-# List current clusters (OPTIONAL) and could be used to determine the next command e.g. create,restart,terminate etc
-curl -X GET \
-    -H "Authorization: Bearer $token" \
-    -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
-    -H "X-Databricks-Azure-Workspace-Resource-Id: $workspaceId" \
-    https://$workspaceUrl/api/2.0/clusters/list
+replaceSource="./data_generation"
+replaceDest=""
 
-# Create Cluster config from variables 
+# Locate the directory for the databricks artifacts 
+find . -type d -name "data_generation" -print0 | while IFS= read -r -d '' dirPath; do
+    echo "Processing directory: $dirPath"
+done
+
+find $dirPath -type f -name "*DS*" -print0 | while IFS= read -r -d '' file; do
+        echo "Processing file: $file"
+        filename=${file//$replaceSource/$replaceDest}
+        # echo "New filename: $filename"
+
+        language=""
+        if [[ "$filename" == *sql ]]
+        then
+            language="SQL"
+        fi
+
+        if [[ "$filename" == *scala ]]
+        then
+            language="SCALA"
+        fi
+
+        if [[ "$filename" == *py ]]
+        then
+            language="PYTHON"
+        fi
+
+        if [[ "$filename" == *r ]]
+        then
+            language="R"
+        fi
+
+        echo "curl -F language=$language -F path=$pathOnDatabricks/$filename -F content=@$file https://$workspaceUrl/api/2.0/workspace/import"
+
+        curl -n https://$workspaceUrl/api/2.0/workspace/import \
+            -H "Authorization:Bearer $token" \
+            -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
+            -H "X-Databricks-Azure-Workspace-Resource-Id: $workspaceId" \
+            -F language="$language" \
+            -F overwrite=true \
+            -F path="$pathOnDatabricks/$filename" \
+            -F content=@"$file"       
+
+        echo ""
+
+    done
+
+# Create Databricks Cluster Config 
 JSON_STRING=$( jq -n -c \
                 --arg cn "$DATABRICKS_CLUSTER_NAME" \
                 --arg sv "$DATABRICKS_SPARK_VERSION" \
@@ -128,7 +198,7 @@ JSON_STRING=$( jq -n -c \
                 autotermination_minutes: ($at|tonumber),
                 spark_conf: ($sc|fromjson)}' )
 
-echo "Creating cluster ........" 
+echo "Creating Databricks Cluster ........" 
 
 # Create a new Cluster
 # Reference: https://docs.microsoft.com/en-us/azure/databricks/dev-tools/api/latest/
