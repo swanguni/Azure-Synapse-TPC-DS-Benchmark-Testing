20844834379ad586ec6c7dc81ca864d43c929371
Stan Wang <stanley_wang_uni@yahoo.com>
2022-03-10 16:11:11 +0000

    Add files via upload

--- Labs/Module 2/databricksDeployment.sh
+++ Labs/Module 2/databricksDeployment.sh
@@ -5,7 +5,7 @@ export ARM_SUBSCRIPTION_ID=$(az account show --query id --output tsv 2>&1)
 export AZURE_DATABRICKS_APP_ID="2ff814a6-3304-4ab8-85cb-cd0e6f879c1d" 
 
 # Key Vault Secret Info
-KEY_VAULT="tpcds-poc-kv"
+KEY_VAULT="tpcds-kv"
 ARM_SPN_CREDENTIAL="tpcds-spn-secret"
 ARM_SPN_OBJECT="tpcds-spn-object"
 ARM_SPN_CLIENT="tpcds-spn-client"
@@ -24,49 +24,14 @@ DATABRICKS_SPARK_CONF='{"spark.speculation":"true","spark.databricks.delta.previ
 DATABRICKS_AUTO_TERMINATE_MINUTES=60
 DATABRICKS_CLUSTER_ID="tpcds-db-cluster-id"
 
-echo "Creating Service Principal ......"
+echo "Retrieving Service Principal Info ......"
 
-APP_SPN_NAME="App-PoC-TPCDS-Test"
-#ARM_CLIENT_SECRET=$(az ad sp create-for-rbac --name "$APP_SPN_NAME" --scopes /subscriptions/"$ARM_SUBSCRIPTION_ID" --query password -o tsv)
-ARM_CLIENT_SECRET="xkl7Q~3tTCkaIAPraAdIqJ_LRyWz66LgOtxAA"
+APP_SPN_NAME="pocapp-tpcds"
 
-ARM_TENANT_ID=$(az ad sp list --display-name "$APP_SPN_NAME" --query [].appOwnerTenantId -o tsv)
-ARM_CLIENT_ID=$(az ad sp list --display-name "$APP_SPN_NAME" --query [].appId -o tsv)
-ARM_OBJECT_ID=$(az ad sp list --display-name "$APP_SPN_NAME" --query [].objectId -o tsv)
-
-echo "Creating Key Vault ......"
-
-if [[ $(az keyvault list --resource-group $RESOURCE_GROUP | jq .[].name | grep -w $KEY_VAULT) != $KEY_VAULT ]]; then
-    az keyvault create --name $KEY_VAULT --resource-group $RESOURCE_GROUP --location $LOCATION
-fi
-
-echo "ARM_CLIENT_SECRET : $ARM_CLIENT_SECRET"
-
-az keyvault secret set  --name $ARM_SPN_CREDENTIAL --value $ARM_CLIENT_SECRET --vault-name $KEY_VAULT
-
-az keyvault secret set --name $ARM_SPN_OBJECT --value $ARM_OBJECT_ID --vault-name $KEY_VAULT
-
-az keyvault secret set --name $ARM_SPN_CLIENT --value $ARM_CLIENT_ID --vault-name $KEY_VAULT
-
-az keyvault secret set --name $ARM_SPN_TENANT --value $ARM_TENANT_ID --vault-name $KEY_VAULT
-
-
-echo "ARM_OBJECT_ID : $ARM_OBJECT_ID"
-echo "$(az keyvault secret show --name $ARM_SPN_OBJECT --vault-name $KEY_VAULT --query value -o tsv)"
-
-echo "ARM_CLIENT_ID : $ARM_CLIENT_ID"
-echo "$(az keyvault secret show --name $ARM_SPN_CLIENT --vault-name $KEY_VAULT --query value -o tsv)"
-
-echo "ARM_TENANT_ID : $ARM_TENANT_ID"
-echo "$(az keyvault secret show --name $ARM_SPN_TENANT --vault-name $KEY_VAULT --query value -o tsv)"
-
-echo "ARM_CLIENT_SECRET : $ARM_CLIENT_SECRET"
-echo "$(az keyvault secret show --name $ARM_SPN_CREDENTIAL --vault-name $KEY_VAULT --query value -o tsv)"
-
-echo "Assign the Resource Group Contributor Role to SPN"
-az role assignment create --assignee "$ARM_OBJECT_ID" \
---role "Contributor" \
---scope "/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP"
+ARM_OBJECT_ID=$(az keyvault secret show --name $ARM_SPN_OBJECT --vault-name $KEY_VAULT --query value -o tsv)
+ARM_CLIENT_ID=$(az keyvault secret show --name $ARM_SPN_CLIENT --vault-name $KEY_VAULT --query value -o tsv)
+ARM_TENANT_ID=$(az keyvault secret show --name $ARM_SPN_TENANT --vault-name $KEY_VAULT --query value -o tsv)
+ARM_CLIENT_SECRET=$(az keyvault secret show --name $ARM_SPN_CREDENTIAL --vault-name $KEY_VAULT --query value -o tsv)
 
 # Login using service principle
 echo "Logging in using Azure Service Pricipal ..... "
