e95febd6a5102a84db3f63b8071a19f7d89255a6
Stan Wang <stanley_wang_uni@yahoo.com>
2022-03-11 16:35:50 -0500

    Update main.tf

--- Labs/Module 1/Terraform/main.tf
+++ Labs/Module 1/Terraform/main.tf
@@ -488,29 +488,3 @@ resource "azurerm_private_endpoint" "synapse-dev" {
     is_manual_connection           = false
   }
 }
-
-resource "azurerm_key_vault" "kv" {
-  name                        = var.keyvault_name
-  location                    = var.azure_region
-  resource_group_name         = var.resource_group_name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-  sku_name = "standard"
-
-  depends_on = [ azurerm_resource_group.resource_group ]
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    secret_permissions = [
-      "Set",
-      "List",
-      "Get",
-      "Delete",
-      "Purge",
-      "Recover"
-    ]
-  }
-}
